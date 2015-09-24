0c5622ba1453b6ccb11e1bf053affce718474953
popovychs <popovychs@bbs-macbook.local>
2015-09-24 11:22:45 +0300

    Initial Commit

---
+++ SPArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SPArray
+//
+//  Created by popovychs on 24.09.15.
+//  Copyright © 2015 popovychs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
